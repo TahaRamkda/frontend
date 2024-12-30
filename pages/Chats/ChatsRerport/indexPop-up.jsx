99c386267afabcabac69c55ad09709d4716697c3
Abbass Dairy <Abbas.dairy@consulttechies.com>
2024-12-30 10:20:35 +0530

    Commit

--- pages/Chats/ChatsRerport/indexPop-up.jsx
+++ pages/Chats/ChatsRerport/indexPop-up.jsx
@@ -13,81 +13,104 @@ const MessageSummary = () => {
     return (
       <App>
         
-    {loading && <Loading />}
+        <Col xxl="3" xl="4" md="5" className="box-col-5 p-0" style={{ height: '100vh' }}>
+            <Card className="left-sidebar-wrapper h-100">
+              <div className="left-sidebar-chat p-3">
+                <InputGroup>
+                  <InputGroupText className="w-full">
+                    <i className="fa fa-search mr-2" aria-hidden="true"></i>
+                    <Input type="text" placeholder="Search here" />
+                  </InputGroupText>
+                </InputGroup>
+              </div>
+              <Nav tabs className="border-tab bg-white border-b border-gray-200 shadow-sm">
+                <NavItem className="flex justify-content-center w-full text-xl font-bold text-gray-900">
+                  Chats
+                </NavItem>
+              </Nav>
+              <TabContent>
+              <TabPane>
+              <ul className="divide-y divide-gray-200 chats-user overflow-y-auto">
+  {loading && <div className="text-center">Please wait while we load your chats..!!</div>}
+  {AgentConversaton?.map((conversation) => (
+    <li
+      key={conversation.id}
+      className={`flex justify-between cursor-pointer px-4 py-0.5 rounded-lg transition-all duration-200 ease-in-out ${
+        Activechat === conversation.id ? 'bg-gray-200' : 'hover:bg-gray-100'
+      }`}
+      onClick={() => {
+        HandleConversationDetail(conversation.id);
+      }}
+      style={{ height: '100px' }} // Height adjustment for the tile-like look
+    >
+      <div className="flex items-center space-x-4 w-full">
+        <div className="relative">
+          <img
+            src={`${BASE_URL}${conversation.logo}`}
+            alt="User Logo"
+            className="w-10 h-10 bg-gray-300 rounded-full object-cover"
+          />
+          {conversation.unreadCount > 0 && (
+            <span className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 bg-green-500 text-white text-xs font-bold rounded-full">
+              {conversation.unreadCount}
+            </span>
+          )}
+        </div>
+        <div className="text-left flex-grow" style={{ minWidth: '0' }}>
+          <span
+            className="block font-medium text-gray-800"
+            style={{
+              width: '200px',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              marginBottom: '2px', // Further reduced margin for spacing between fullName and phoneNumber
+            }}
+          >
+            {conversation.fullName}
+          </span>
+          <span
+            className="block text-sm text-gray-600"
+            style={{
+              marginBottom: '2px', // Further reduced margin for spacing between phoneNumber and lastMessageText
+            }}
+          >
+            {conversation.phoneNumber}
+          </span>
 
-           <ul className="divide-y divide-gray-200 chats-user overflow-y-auto">
-            {loading && <div className="text-center">Please wait while we load your chats..!!</div>}
-            {AgentConversaton?.map((conversation) => (
-              <li
-                key={conversation.id}
-                className={`flex justify-between cursor-pointer px-4 py-0.5 rounded-lg transition-all duration-200 ease-in-out ${
-                  Activechat === conversation.id ? 'bg-gray-200' : 'hover:bg-gray-100'
-                }`}
-                onClick={() => {
-                  HandleConversationDetail(conversation.id);
-                }}
-                style={{ height: '100px' }} // Height adjustment for the tile-like look
-              >
-                <div className="flex items-center space-x-4 w-full">
-                  <div className="relative">
-                    <img
-                      src={`${BASE_URL}${conversation.logo}`}
-                      alt="User Logo"
-                      className="w-10 h-10 bg-gray-300 rounded-full object-cover"
-                    />
-                    {conversation.unreadCount > 0 && (
-                      <span className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 bg-green-500 text-white text-xs font-bold rounded-full">
-                        {conversation.unreadCount}
-                      </span>
-                    )}
-                  </div>
-                  <div className="text-left flex-grow" style={{ minWidth: '0' }}>
-                    <span
-                      className="block font-medium text-gray-800"
-                      style={{
-                        width: '200px',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                        marginBottom: '2px', // Further reduced margin for spacing between fullName and phoneNumber
-                      }}
-                    >
-                      {conversation.fullName}
-                    </span>
-                    <span
-                      className="block text-sm text-gray-600"
-                      style={{
-                        marginBottom: '2px', // Further reduced margin for spacing between phoneNumber and lastMessageText
-                      }}
-                    >
-                      {conversation.phoneNumber}
-                    </span>
-          
-                    {conversation.lastMessageText !== '' ? (
-                      <p
-                        className="block text-sm text-gray-500 mt-0"
-                        style={{
-                          width: '220px', // Adjusted width for better tile look
-                          overflow: 'hidden',
-                          textOverflow: 'ellipsis',
-                          whiteSpace: 'nowrap',
-                          marginBottom: '0', // Further reduced margin to avoid extra space between lastMessageText and next element
-                        }}
-                      >
-                        {conversation.lastMessageText}
-                      </p>
-                    ) : (
-                      <div className="flex items-center mt-0">
-                        <i className="fa fa-photo mr-2 text-gray-500"></i>
-                        <p className="block text-sm text-gray-500">Media</p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-          
+          {conversation.lastMessageText !== '' ? (
+            <p
+              className="block text-sm text-gray-500 mt-0"
+              style={{
+                width: '220px', // Adjusted width for better tile look
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                marginBottom: '0', // Further reduced margin to avoid extra space between lastMessageText and next element
+              }}
+            >
+              {conversation.lastMessageText}
+            </p>
+          ) : (
+            <div className="flex items-center mt-0">
+              <i className="fa fa-photo mr-2 text-gray-500"></i>
+              <p className="block text-sm text-gray-500">Media</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </li>
+  ))}
+</ul>
+
+
+    </TabPane>
+
+
+
+              </TabContent>
+            </Card>
+          </Col>
       
       </App>
    
--- pages/Supervisor/ChatsMonitor/index.jsx
+++ pages/Supervisor/ChatsMonitor/index.jsx
@@ -16,14 +16,7 @@ import App from '@/components/App';
 const ChatsReport = () => {
   const dispatch = useDispatch();
   const [senderid, setsenderid] = useState(0);
-   const [Activechat, setActiveChat] = useState(0);
    const [DetailModal, setDetailModal]= useState(false);
-   const agentChatRef = useRef([AgentConversaton]);
-  
-  
- 
-  const [isfilteropen, setisfilteropen] = useState(false);
- const [showfilterbutton, setshowfilterbutton] = useState(true);
   const { chatsMonitor, loading, error, currentPage, pageSize, totalRecords } = useSelector((state) => state.Supervisor);
   const [clientId, setClientId] = useState(null);
 
@@ -181,81 +174,7 @@ const ChatsReport = () => {
             },
           }}
         />
-      {DetailModal&&(
-         <ul className="divide-y divide-gray-200 chats-user overflow-y-auto">
-          {loading && <div className="text-center">Please wait while we load your chats..!!</div>}
-          {AgentConversaton?.map((conversation) => (
-            <li
-              key={conversation.id}
-              className={`flex justify-between cursor-pointer px-4 py-0.5 rounded-lg transition-all duration-200 ease-in-out ${
-                Activechat === conversation.id ? 'bg-gray-200' : 'hover:bg-gray-100'
-              }`}
-              onClick={() => {
-                HandleConversationDetail(conversation.id);
-              }}
-              style={{ height: '100px' }} // Height adjustment for the tile-like look
-            >
-              <div className="flex items-center space-x-4 w-full">
-                <div className="relative">
-                  <img
-                    src={`${BASE_URL}${conversation.logo}`}
-                    alt="User Logo"
-                    className="w-10 h-10 bg-gray-300 rounded-full object-cover"
-                  />
-                  {conversation.unreadCount > 0 && (
-                    <span className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 bg-green-500 text-white text-xs font-bold rounded-full">
-                      {conversation.unreadCount}
-                    </span>
-                  )}
-                </div>
-                <div className="text-left flex-grow" style={{ minWidth: '0' }}>
-                  <span
-                    className="block font-medium text-gray-800"
-                    style={{
-                      width: '200px',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                      marginBottom: '2px', // Further reduced margin for spacing between fullName and phoneNumber
-                    }}
-                  >
-                    {conversation.fullName}
-                  </span>
-                  <span
-                    className="block text-sm text-gray-600"
-                    style={{
-                      marginBottom: '2px', // Further reduced margin for spacing between phoneNumber and lastMessageText
-                    }}
-                  >
-                    {conversation.phoneNumber}
-                  </span>
-        
-                  {conversation.lastMessageText !== '' ? (
-                    <p
-                      className="block text-sm text-gray-500 mt-0"
-                      style={{
-                        width: '220px', // Adjusted width for better tile look
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                        marginBottom: '0', // Further reduced margin to avoid extra space between lastMessageText and next element
-                      }}
-                    >
-                      {conversation.lastMessageText}
-                    </p>
-                  ) : (
-                    <div className="flex items-center mt-0">
-                      <i className="fa fa-photo mr-2 text-gray-500"></i>
-                      <p className="block text-sm text-gray-500">Media</p>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        
-      )}
+      
     </App>
  
   );
