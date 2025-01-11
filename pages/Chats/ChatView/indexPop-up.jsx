99197cbbd7da3c27c38627d86397b588276187fd
Abbass Dairy <Abbas.dairy@consulttechies.com>
2025-01-11 17:29:52 +0530

    by ad

--- pages/Chats/ChatView/indexPop-up.jsx
+++ pages/Chats/ChatView/indexPop-up.jsx
@@ -27,6 +27,7 @@ import {
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { BASE_URL } from "@/utils/apiConstants";
 import Loader from "@/components/Loader";
+
 const Chatview = ({ ChatId, onClose, isVisible }) => {
   const dispatch = useDispatch();
   const [Activechat, setActiveChat] = useState(0);
@@ -60,15 +61,15 @@ const Chatview = ({ ChatId, onClose, isVisible }) => {
     <App>
       <Modal isOpen={isVisible} toggle={onClose} fade={false}>
         <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50 ">
-          <div className="bg-white p-6 rounded shadow-lg w-2/5 relative h-2/3">
+          <div className="bg-white p-6 rounded shadow-lg w-2/5 relative h-auto max-h-[80vh]">
             <ModalHeader toggle={onClose}></ModalHeader>
             <ModalBody>
-              <div className="right-sidebar-chat ">
+              <div className="right-sidebar-chat">
                 <div
-                  className="right-sidebar-chat p-4 w-full height-chat-box overflow-y-auto chat-background "
-                  style={{ height: "569px" }}
+                  className="right-sidebar-chat p-4 w-full overflow-y-auto chat-background"
+                  style={{ maxHeight: "70vh", overflowY: "auto" }}
                 >
-                  <div className="msger flex flex-col ">
+                  <div className="msger flex flex-col">
                     <div className="msger-chat flex-grow overflow-y-auto space-y-4 px-4 py-2">
                       {loading && (
                         <div className="text-center">
