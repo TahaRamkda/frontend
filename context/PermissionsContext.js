ae72b4a9d6c0a086de030518f4832b7292401883
Hussain <h.janglate@oginnovation.com>
2025-03-26 12:19:43 +0300

    added template dropdown in flow visualisation

--- context/PermissionsContext.js
+++ context/PermissionsContext.js
@@ -1,11 +1,36 @@
-// PermissionsContext.js
+// context/PermissionsContext.js
 import React, { createContext, useContext } from 'react';
 
 const PermissionsContext = createContext();
 
-export const usePermissions = () => useContext(PermissionsContext);
+export const usePermissions = () => {
+  const permissions = useContext(PermissionsContext);
 
-export const PermissionsProvider = ({ permissions, children }) => {
+  const hasPermission = (taskName, action) => {
+    if (!permissions || !Array.isArray(permissions)) return false;
+    const permission = permissions.find(
+      (perm) => perm.permissionTaskName.toLowerCase() === taskName.toLowerCase()
+    );
+    if (!permission) return false;
+
+    switch (action.toLowerCase()) {
+      case "view":
+        return permission.canView;
+      case "create":
+        return permission.canCreate;
+      case "update":
+        return permission.canUpdate;
+      case "delete":
+        return permission.canDelete;
+      default:
+        return false;
+    }
+  };
+
+  return { permissions, hasPermission };
+};
+
+export const PermissionsProvider = ({ permissions = [], children }) => {
   return (
     <PermissionsContext.Provider value={permissions}>
       {children}
--- utils/constants.js
+++ utils/constants.js
@@ -74,10 +74,10 @@ export const ExpireTime_Message_TRY5=60000 ;
 
 export const HeartBeat_Interval=100000 ; // 5 minutes in milliseconds
 
-export const ProdAppId = "a06956a9-aab6-4bff-9465-dc19f5f2a33b";
+export const AppId = "a06956a9-aab6-4bff-9465-dc19f5f2a33b";
 
-export const AppId="2b6362f1-b706-4087-bfaf-0d625c02110b";
+export const StagAppId="2b6362f1-b706-4087-bfaf-0d625c02110b";
 
-export const API_KEY1= "os_v2_app_fnrwf4nxazaipp5pbvrfyaqrbm5fgnr5ontuwrfsy2no57l3fxtwbtfinybm4x2ivawfnmztyenpcllfjijcisl774siswalccqwt7i";
+export const API_KEY= "os_v2_app_fnrwf4nxazaipp5pbvrfyaqrbm5fgnr5ontuwrfsy2no57l3fxtwbtfinybm4x2ivawfnmztyenpcllfjijcisl774siswalccqwt7i";
 
-export const API_KEY = "os_v2_app_ubuvnknkwzf77fdf3qm7l4vdhng2wrzqbmhupyn4g22egnsgjxyon5bmd7z67msxiqtxenrk5ghe3cayc5zc6nmeqchu36ykk7m7k6q";
+export const API_KEY1 = "os_v2_app_ubuvnknkwzf77fdf3qm7l4vdhng2wrzqbmhupyn4g22egnsgjxyon5bmd7z67msxiqtxenrk5ghe3cayc5zc6nmeqchu36ykk7m7k6q";
