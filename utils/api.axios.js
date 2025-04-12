e43f108739801e0ba8fbd1a65cd95202afc6254b
Hussain <h.janglate@oginnovation.com>
2025-04-12 16:06:20 +0300

    commit

--- utils/api.axios.js
+++ utils/api.axios.js
@@ -11,7 +11,7 @@ const logger = new Logger();
 
 instance.interceptors.request.use(
   async (config) => {
-    debugger
+    
     
     if (typeof window !== 'undefined') {
       const accessToken = localStorage.getItem('accessToken');
