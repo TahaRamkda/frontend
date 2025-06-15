4e60e9303f00b595d195275652bde81f05538f3b
Abbas Dairy <Abbas.dairy@consulttechies.com>
2025-06-15 14:03:21 +0300

    commit

--- components/FavIconManager/index.jsx
+++ components/FavIconManager/index.jsx
@@ -3,16 +3,8 @@ import { useEffect } from "react";
 
 const FaviconManager = () => {
   useEffect(() => {
-    const hostname = window.location.hostname;
-
-    let faviconMap = {};
-    try {
-      faviconMap = JSON.parse(process.env.NEXT_PUBLIC_FAVICON_MAP );
-    } catch (err) {
-      console.error("Invalid favicon map in .env:", err);
-    }
-
-    const faviconHref = faviconMap[hostname] || "/favicons/default.ico";
+    // Read favicon path from localStorage
+    const faviconPath = localStorage.getItem("LogoPath") || "/favicons.ico";
 
     // Remove existing favicons
     document.querySelectorAll("link[rel*='icon']").forEach((el) => el.remove());
@@ -20,7 +12,7 @@ const FaviconManager = () => {
     // Inject new favicon
     const link = document.createElement("link");
     link.rel = "icon";
-    link.href = faviconHref;
+    link.href = faviconPath;
     document.head.appendChild(link);
   }, []);
 
